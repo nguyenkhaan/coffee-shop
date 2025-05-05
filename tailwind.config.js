e1726cafd3b1cff2f7b65377ec71fde8f330d4a2
khaan <nguyenkhaan2006@gmail.com>
2025-05-05 11:26:50 +0700

    complete_header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-coffe-color': '#C89F94' 
+      }
+    },
   },
   variants: {
     extend: {},
